bd3f8037d6b3b73b3015efc200bfaee0d331d875
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-04 09:54:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["cat", "dog", "rabbit", "chameleon"]
+  ["dodo", "sea cow", "mammoth", "ibex"]
 end
 
 def add_element_to_end_of_array(array, element)
